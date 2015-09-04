64e0990815964c166f819c79ea46d4cde5603e43
linlin <wllrenxue@163.com>
2015-09-04 21:03:47 +0800

    2nd stage - parepared pure and random format (test and train) for boost classification. move from svr 3 to svr 2 since 3 has no icsi

--- text-stress/src/one_create_pure_sentence_label.py
+++ text-stress/src/one_create_pure_sentence_label.py
@@ -11,9 +11,11 @@ if __name__=="__main__":
 
 	this_file_path = os.path.abspath( sys.argv[0])
 	src_abs_path =  os.path.dirname(this_file_path) 
-	logFile = src_abs_path + '/logFile.txt'
-
-	logging.basicConfig(filename= logFile, level = logging.WARNING)
+	root_dir = os.path.dirname(src_abs_path)
+	logFile = root_dir + '/logFile.txt'
+	os.system('rm %s' %logFile)
+	logging.basicConfig(filename= logFile, level = logging.NOTSET)
 
 	lex = PureSentLabelCls()
+	lex.randomize()
 	lex.getPureSentLabel()
